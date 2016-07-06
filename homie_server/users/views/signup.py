03f6c35abc469840445f8d50e677ab2f2ad65683
message0316 <message0316@gmail.com>
2016-07-06 19:33:00 +0900

    init SignupView: render users/signup.html template

---
+++ homie_server/users/views/signup.py
@@ -0,0 +1,11 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+class SignupView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "users/signup.html",
+            context={},
+        )
