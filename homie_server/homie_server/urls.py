1d33d65d77afb58ac77aaff909930f900cc27636
message0316 <message0316@gmail.com>
2016-07-15 14:54:38 +0900

    update header.html: add login, singup link

--- homie_server/homie_server/urls.py
+++ homie_server/homie_server/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 
 from users.views import *
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^signup/', SignupView.as_view(), name="signup"),
+    url(r'^login/', LoginView.as_view(), name="login"),
 ]
