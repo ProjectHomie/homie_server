2bf1602e21fb261af125854797d785c118daccda
message0316 <message0316@gmail.com>
2016-07-15 11:28:08 +0900

    usermodel custom

--- homie_server/users/models/user.py
+++ homie_server/users/models/user.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    
+
     phonenumber = models.CharField(
         max_length=16,
         blank=True,
@@ -15,12 +15,12 @@ class User(AbstractUser):
         blank=True,
         null=True,
     )
-    
+
     address = models.CharField(
         max_length=128,
         blank=True,
         null=True,
     )
-    
+
     def __strdd__(self):
         return self.phonenumber
