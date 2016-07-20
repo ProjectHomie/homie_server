79c725e134d61450bf25b97e79e24adfd49972c2
message0316 <message0316@gmail.com>
2016-07-20 10:59:29 +0900

    init Post model: Post has a title, and content

---
+++ homie_server/posts/models/post.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=120,
+    )
+
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
