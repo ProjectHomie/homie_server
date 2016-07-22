9eb00f697125cdeb473b897d1f2cf8e271928ee1
message0316 <message0316@gmail.com>
2016-07-22 21:15:27 +0900

    refactor MypageView: using TemplateView

--- homie_server/users/views/mypage.py
+++ homie_server/users/views/mypage.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class MypageView(LoginRequiredMixin, View):
-
-    def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            "users/mypage.html",
-            context={},
-        )
+class MypageView(LoginRequiredMixin, TemplateView):
+    template_name = "users/mypage.html"
+# class MypageView(LoginRequiredMixin, View):
+#     def get(self, request, *args, **kwargs):
+#         return render(
+#             request,
+#             "users/mypage.html",
+#             context={},
+#         )
 # @login_required
 # def mypage(request):
 #     return render(
