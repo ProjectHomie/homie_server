c3e3acf6e40fb2e6da3cb6852df677912a79ab45
message0316 <message0316@gmail.com>
2016-07-25 11:13:31 +0900

    update PosrCreateView: via CreateView

--- homie_server/posts/views/create.py
+++ homie_server/posts/views/create.py
@@ -1,19 +1,24 @@
-from django.views.generic import View
+from django.views.generic.edit import CreateView
 from django.shortcuts import redirect
 
-from posts.models import Post
+from .base import PostBaseView
 
 
-class PostCreateView(View):
+class PostCreateView(PostBaseView, CreateView):
+    fields = [
+        "title",
+        "content",
+    ]
 
-    def post(self, request, *arg, **kwargs):
-        title = request.POST.get("title")
-        content = request.POST.get("content")
-
-        post = Post.objects.create(
-            user=request.user,
-            title=title,
-            content=content,
-        )
-
-        return redirect(post)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+#     def post(self, request, *arg, **kwargs):
+#         title = request.POST.get("title")
+#         content = request.POST.get("content")
+#         post = Post.objects.create(
+#             user=request.user,
+#             title=title,
+#             content=content,
+#         )
+#         return redirect(post)
