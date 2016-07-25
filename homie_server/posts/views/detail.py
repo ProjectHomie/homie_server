6f33393d5cae878cccee5fdfa8ef7d59d75a1221
message0316 <message0316@gmail.com>
2016-07-25 09:54:29 +0900

    refactor PostDetailView

--- homie_server/posts/views/detail.py
+++ homie_server/posts/views/detail.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 from posts.models import Post
 
 
-def detail(request, post_id):
-    return render(
-        request,
-        "posts/detail.html",
-        context={
-            "post": Post.objects.get(id=post_id),
-        },
-    )
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/detail.html"
+# def detail(request, post_id):
+#     return render(
+#         request,
+#         "posts/detail.html",
+#         context={
+#             "post": Post.objects.get(id=post_id),
+#         },
+#     )
