f39766981b6464ba14bc8bb1d6e71f06eb32c7fe
message0316 <message0316@gmail.com>
2016-07-29 07:01:01 +0900

    exit

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: python homie_server/manage.py runserver 8000
-worker: celery --workdir=homie_server/ --app=homie_server.celery:app --concurrency=3 worker
-flower: celery --workdir=homie_server/ --app=homie_server.celery:app flower
--- homie_server/posts/models/post.py
+++ homie_server/posts/models/post.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from users.models import User
 
 
-# class PostManager(models.Manager):
-#     def public(self):
-#         return self.filter(is_public=True)
+class PostManager(models.Manager):
+    def public(self):
+        return self.filter(is_public=True)
+
+
 class Post(models.Model):
+
+    user = models.ForeignKey(User)
+
+    hash_id = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+    )
+
     title = models.CharField(
-        max_length=120,
+        max_length=256,
     )
 
     content = models.TextField()
+
     is_public = models.BooleanField(
         default=True,
     )
@@ -20,17 +33,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-#     def get_absolute_url(self):
-#         return reverse(
-#             "post:detail",
-#             kwargs={
-#                 "pk": self.id,
-#             },
-#         )
-#     def get_update_url(self):
-#         return reverse(
-#             "posts:update",
-#             kwargs={
-#                 "post_id": self.id,
-#             }
-#         )
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:detail",
+            kwargs={
+                "slug": self.hash_id,
+            },
+        )
+
+    def get_update_url(self):
+        return reverse(
+            "posts:update",
+            kwargs={
+                "post_id": self.id,
+            }
+        )
