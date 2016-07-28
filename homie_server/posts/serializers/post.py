42aa43fa9e16110dd799a079be9a42def281018e
message0316 <message0316@gmail.com>
2016-07-28 14:51:12 +0900

    update PostModelSerializer: username

--- homie_server/posts/serializers/post.py
+++ homie_server/posts/serializers/post.py
@@ -1,13 +1,18 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from posts.models import Post
 
 
-class PostModelSerializer(ModelSerializer):
+class PostModelSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(source="user.username",)
 
     class Meta:
         model = Post
         fields = [
             "title",
             "content",
+
+            "username",
         ]
